1ac099dcc597d05ac33debe2609e4cf399670265
chz <chz@bingularity.org>
2011-04-10 21:23:45 -0700

    more minix tests

--- tests/tests_extra.minix.sh
+++ tests/tests_extra.minix.sh
@@ -19,6 +19,30 @@ fs_reloc_startup_img $fs $imgname
 fsck_img $imgname
 fs_scan_startup_img $fs $imgname
 
+imgname=$fs-scatter.img
+cp ${src_root}/img/$fs-many.img ${src_root}/img/$imgname
+fs_scatter_startup_img $fs $imgname
+fsck_img $imgname
+fs_scan_startup_img $fs $imgname
+
+imgname=$fs-thrashcopy.img
+cp ${src_root}/img/$fs-scatter.img ${src_root}/img/$imgname
+fs_thrashcopy_startup_img $fs $imgname
+fsck_img $imgname
+fs_scan_startup_img $fs $imgname
+
+imgname=$fs-defrag.img
+cp ${src_root}/img/$fs-scatter.img ${src_root}/img/$imgname
+fs_defrag_startup_img $fs $imgname
+fsck_img $imgname
+fs_scan_startup_img $fs $imgname
+
+imgname=$fs-smush.img
+cp ${src_root}/img/$fs-defrag.img ${src_root}/img/$imgname
+fs_smush_startup_img $fs $imgname
+fsck_img $imgname
+fs_scan_startup_img $fs $imgname
+
 do_fsck_test "printf '\xff\xff' >sb/s_log_zone_size" "chk_1k_zones" "minix-many.img"
 do_fsck_test "printf '\x0\x0' >sb/s_imap_blocks" "chk_bad_s_imap_blocks" "minix-many.img"
 do_fsck_test "printf '\x0\x0' >sb/s_zmap_blocks" "chk_bad_s_zmap_blocks" "minix-many.img"
--- util/draw.minix.sh
+++ util/draw.minix.sh
@@ -7,16 +7,16 @@ MINIX_REGEX="(_inode|super_block|dir_entry|omap|zmap|file_block)"
 function draw_scan_minix
 {
 	draw_scan_fs minix "${MINIX_REGEX}"
-	draw_scan_fs_img minix "minix-many.img" "$MINIX_REGEX"
-	draw_scan_fs_img minix "minix-depth.img" "$MINIX_REGEX"
-	draw_scan_fs_img minix "minix-relocate.img" "$MINIX_REGEX"
+	for ipath in img/minix-*.img; do
+		fname=`echo $ipath | cut -f2 -d'/'`
+		draw_scan_fs_img minix "$fname" "$MINIX_REGEX"
+	done
 }
 
 function draw_minix_img
 {
 	draw_fs_img minix "$1" "${MINIX_REGEX}"
 }
-
 
 if [ -z "$1" ]; then
 	draw_scan_minix
